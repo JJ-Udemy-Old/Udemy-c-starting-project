f4741528577d1536fd5b33b3c2c43eb9505a7db0
weiwei0304 <106504956+weiwei0304@users.noreply.github.com>
2023-11-08 23:15:40 +0800

    course 49

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,8 @@
-// import image
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './Data';
 
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescription[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.description} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
-
-/* better cleaner way */
-// function CoreConcept({ image, title, description }) {
-//   return (
-//     <li>
-//       <img src={image} alt={description} />
-//       <h3>{title}</h3>
-//       <p>{description}</p>
-//     </li>
-//   );
-// }
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -61,6 +22,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
